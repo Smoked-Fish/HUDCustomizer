79df42dd5ac00c35c510a83ee8ef4fb8a4e63e20
Smoked Fish <espy.jacob@gmail.com>
2024-05-06 20:31:45 -0700

    rework to work with multiple patch files

--- Framework/Patches/PatchTemplate.cs
+++ Framework/Patches/PatchTemplate.cs
@@ -8,11 +8,14 @@ namespace HUDCustomizer.Framework.Patches
     internal class PatchTemplate
     {
         internal static Harmony _harmony;
+        internal static Type _object;
 
-        internal PatchTemplate(Harmony modHarmony)
+        internal PatchTemplate(Harmony modHarmony, Type objectType)
         {
             _harmony = modHarmony;
+            _object = objectType;
         }
+
 
         /// <summary>
         /// Applies method patches using Harmony for a specified target method.
@@ -21,20 +24,20 @@ namespace HUDCustomizer.Framework.Patches
         /// <param name="originalMethod">The name of the original method to patch.</param>
         /// <param name="newMethod">The name of the method to be applied as a patch.</param>
         /// <param name="parameters">Optional parameters for the method.</param>
-        public void Patch(PatchType patchType, Type objectType, string originalMethod, string newMethod, Type[] parameters = null)
+        public void Patch(PatchType patchType, string originalMethod, string newMethod, Type[] parameters = null)
         {
             try
             {
                 switch (patchType)
                 {
                     case PatchType.Prefix:
-                        _harmony.Patch(AccessTools.Method(objectType, originalMethod, parameters), prefix: new HarmonyMethod(GetType(), newMethod));
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), prefix: new HarmonyMethod(GetType(), newMethod));
                         break;
                     case PatchType.Postfix:
-                        _harmony.Patch(AccessTools.Method(objectType, originalMethod, parameters), postfix: new HarmonyMethod(GetType(), newMethod));
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), postfix: new HarmonyMethod(GetType(), newMethod));
                         break;
                     case PatchType.Transpiler:
-                        _harmony.Patch(AccessTools.Method(objectType, originalMethod, parameters), transpiler: new HarmonyMethod(GetType(), newMethod));
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), transpiler: new HarmonyMethod(GetType(), newMethod));
                         break;
                     default:
                         ModEntry.monitor.Log($"Unknown patch type: {patchType}", LogLevel.Error);
