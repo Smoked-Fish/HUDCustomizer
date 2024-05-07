d83736033b88482e3240b0696223d58a6d325c91
Smoked Fish <espy.jacob@gmail.com>
2024-05-06 20:29:43 -0700

    add configs

--- ModConfig.cs
+++ ModConfig.cs
@@ -6,7 +6,49 @@ namespace HUDCustomizer
     internal class ModConfig
     {
         public bool EnableMod { get; set; } = true;
-        public bool DisableDayTimeMoneyBox { get; set; } = false;
-        public KeybindList ToggleHudKey {  get; set; } = new KeybindList(SButton.Q);
+
+        public KeybindList ToggleDTMBoxHUD {  get; set; } = new KeybindList(SButton.None);
+        public bool DisableDTMBoxHUD { get; set; } = false;
+        public float DTMBoxScale { get; set; } = 1f;
+
+        public KeybindList ToggleToolbarHUD { get; set; } = new KeybindList(SButton.None);
+        public bool DisableToolbarHUD { get; set; } = false;
+        public float ToolbarScale { get; set; } = 1f;
+
+        public KeybindList ToggleResourceBarsHUD { get; set; } = new KeybindList(SButton.None);
+        public bool DisableResourceBarsHUD { get; set; } = false;
+        public float ResourceBarsScale { get; set; } = 1f;
+
+        public KeybindList ToggleBuffsHUD { get; set; } = new KeybindList(SButton.None);
+        public bool DisableBuffsHUD { get; set; } = false;
+        public float BuffsScale { get; set; } = 1f;
+
+        public ModConfig() 
+        {
+            DTMBoxScale = 1f;
+            ToolbarScale = 1f;
+            ResourceBarsScale = 1f;
+            BuffsScale = 1f;
+        }
+
+        public void ToggleDTMBoxHUDState()
+        {
+            DisableDTMBoxHUD = !DisableDTMBoxHUD;
+        }
+
+        public void ToggleToolbarHUDState()
+        {
+            DisableToolbarHUD = !DisableToolbarHUD;
+        }
+
+        public void ToggleResourceBarsHUDState()
+        {
+            DisableResourceBarsHUD = !DisableResourceBarsHUD;
+        }
+
+        public void ToggleBuffsHUDState()
+        {
+            DisableBuffsHUD = !DisableBuffsHUD;
+        }
     }
 }
