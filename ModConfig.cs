48b3c52018878af370828509c8717dc0fe1f0b96
Smoked Fish <espy.jacob@gmail.com>
2024-05-07 23:39:10 -0700

    temp config

--- ModConfig.cs
+++ ModConfig.cs
@@ -9,7 +9,7 @@ namespace HUDCustomizer
 
         public KeybindList ToggleDTMBoxHUD {  get; set; } = new KeybindList(SButton.None);
         public bool DisableDTMBoxHUD { get; set; } = false;
-        public float DTMBoxScale { get; set; } = 1f;
+        public float DTMBoxScale { get; set; } = 0.75f;
 
         public KeybindList ToggleToolbarHUD { get; set; } = new KeybindList(SButton.None);
         public bool DisableToolbarHUD { get; set; } = false;
@@ -22,14 +22,6 @@ namespace HUDCustomizer
         public KeybindList ToggleBuffsHUD { get; set; } = new KeybindList(SButton.None);
         public bool DisableBuffsHUD { get; set; } = false;
         public float BuffsScale { get; set; } = 1f;
-
-        public ModConfig() 
-        {
-            DTMBoxScale = 1f;
-            ToolbarScale = 1f;
-            ResourceBarsScale = 1f;
-            BuffsScale = 1f;
-        }
 
         public void ToggleDTMBoxHUDState()
         {
